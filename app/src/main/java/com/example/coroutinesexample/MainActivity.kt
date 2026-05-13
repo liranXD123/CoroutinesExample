4d2482d614717d8e0cdd46c5e1fbec7c7f60405b
liranXD123 <liranios123@gmail.com>
2026-05-13 08:04:05 +0300

    job added

--- app/src/main/java/com/example/coroutinesexample/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesexample/MainActivity.kt
@@ -4,15 +4,26 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.coroutinesexample.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.time.delay
 import kotlinx.coroutines.withContext
 import kotlin.concurrent.thread
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var job: Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     companion object{
         private val TAG = MainActivity::class.java.name
@@ -24,25 +35,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        job = Job()
+        val job = launch(start = CoroutineStart.LAZY){
+            fetchAndShowUser()
+        }
         binding.fetchBtn.setOnClickListener {
-            GlobalScope.launch(Dispatchers.Main) {
-                fetchAndShowUser()
+            job.start()
+        }
+        launch{
+            try{
+                delay(Long.MAX_VALUE)
+            }catch (e: Exception){
+                Log.d(TAG,"Coroutine cancelled")
             }
         }
-        /*GlobalScope.launch {
-            println("Default : I'm working in thread ${Thread.currentThread().name}")
-        }
-        GlobalScope.launch(Dispatchers.IO) {
-            println("IO : I'm working in thread ${Thread.currentThread().name}")
-        }
-        GlobalScope.launch(Dispatchers.Main) {
-            println("Main : I'm working in thread ${Thread.currentThread().name}")
-        }
-        GlobalScope.launch(newSingleThreadContext("MyOwnThread")) { // will get its
-            val own = null
-            println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
-        }
-*/
     }
     private suspend fun fetchAndShowUser(){
         val user1 = fetchUserOne()
@@ -52,19 +58,24 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "User two fetched")
         showUser(user1.await(), user2.await())
     }
-    private suspend fun fetchUserOne()= GlobalScope.async(Dispatchers.IO) {
-        Thread.sleep(4000)
+    private fun fetchUserOne()= async(Dispatchers.IO) {
+        delay(4000)
         User("Moshe", "niggersXD123@gmail.com")
     }
 
-    private suspend fun fetchUserTwo()= GlobalScope.async(Dispatchers.IO) {
-        Thread.sleep(4000)
+    private fun fetchUserTwo()= async(Dispatchers.IO) {
+        delay(4000)
         User("Misha", "ballsXD@gmail.com")
     }
 
     private fun showUser(user1: User, user2:User){
         binding.userOneTextview.text = user1.toString()
         binding.userTwoTextview.text = user2.toString()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
 
